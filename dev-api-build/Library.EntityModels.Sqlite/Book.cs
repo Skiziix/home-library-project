6a6563ebbebe43bcb8f4d2d99f516713c24298a0
Cannonball <rearick.seth@gmail.com>
2024-12-29 14:24:31 -0500

    Scaffold orm models for Library project.

---
+++ dev-api-build/Library.EntityModels.Sqlite/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.EntityModels;
+
+[Table("book")]
+public partial class Book
+{
+    [Key]
+    [Column("book_id")]
+    public int BookId { get; set; }
+
+    [Column("author_id", TypeName = "INT")]
+    public int? AuthorId { get; set; }
+
+    [Column("book_title", TypeName = "tinytext")]
+    public string? BookTitle { get; set; }
+
+    [Column("publish_year", TypeName = "year")]
+    public int? PublishYear { get; set; }
+
+    [ForeignKey("AuthorId")]
+    [InverseProperty("Books")]
+    public virtual Author? Author { get; set; }
+}
---
+++ dev-api-build/Library.EntityModels.Sqlite/VRecentBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.EntityModels;
+
+[Keyless]
+public partial class VRecentBook
+{
+    [Column("author_name")]
+    public string? AuthorName { get; set; }
+
+    [Column("book_title", TypeName = "tinytext")]
+    public string? BookTitle { get; set; }
+
+    [Column("publish_year", TypeName = "year")]
+    public int? PublishYear { get; set; }
+}
